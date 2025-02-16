3d67f79fd487837f5c79d2f419af012cfb27898e
Advaithdp <advaithdp@email.com>
2025-02-16 14:44:48 +0530

    INITIAL SET UP" "

---
+++ dcrm/mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+dataBase= mysql.connector.connect(host="localhost",
+                                  user='root',
+                                  passwd="password",
+                                  
+)
+cursorObject=dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE ELDERCO")
+print("all DOne")
